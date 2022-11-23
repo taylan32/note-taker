fea0ee05f3af2fd1fb41d42ff3eacace8bb8fd4b
Taylan <taylanalparslan@hotmail.com>
2022-11-23 12:52:21 +0300

    Initial commit.

---
+++ src/main/java/com/example/nodetaker/NodeTakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nodetaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeTakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeTakerApplication.class, args);
+	}
+
+}
